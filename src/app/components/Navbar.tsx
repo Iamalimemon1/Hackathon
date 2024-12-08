c29197f8803c7f0afcb0e09dc9f331d1d7485337
Ali Jawaid <iamalimemon11@gmail.com>
2024-12-08 17:26:23 +0500

    second

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { MdDone } from "react-icons/md";
-import { GrCart } from "react-icons/gr";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 import { FaAngleDown } from "react-icons/fa6";
 import React from 'react'
 
@@ -10,14 +10,14 @@ function Navbar() {
     <header>
       <div className="container bg-[#272343] w-full max-w-[1920px] h-[45px]">
         <div className="banner flex p-[14px] items-center justify-around gap-2 text-[13px] text-white">
-          <div className="tleft flex items-center">
+          <div className="tleft gap-2 flex items-center">
           <MdDone className='max-w-[16px] max-h-[16px] text-white'/> Free shipping on all orders over $50
           </div>
 
           <div className="tright flex gap-6">
-            <Link href="#" className='flex items-center gap-1'>Eng<FaAngleDown className='text-[13px]'/></Link>
-            <Link href="#">Faq</Link>
-            <Link href="#">Need Help</Link>
+            <Link href="#" className='flex items-center gap-1'>Eng <FaAngleDown className='text-[13px]'/></Link>
+            <Link href="#">Faq </Link>
+            <Link href="#" className='flex items-center gap-1'>Need Help <HiOutlineExclamationCircle /></Link>
           </div>
 
         </div>
